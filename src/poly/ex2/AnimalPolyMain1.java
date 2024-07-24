d061d4d946b0e9227afd2a220c4be7e486068d29
AIM <rolety2202@naver.com>
2024-07-24 14:06:41 +0900

    추상클레스 및 인터페이스

---
+++ src/poly/ex2/AnimalPolyMain1.java
@@ -0,0 +1,18 @@
+package poly.ex2;
+
+public class AnimalPolyMain1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+        sound(dog);
+        sound(cat);
+        sound(caw);
+    }
+
+    private static void sound(Animal animal){
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
